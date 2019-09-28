84caec74eb16c4fc312c0f2941061ec857e48175
Pascal <pa@catatec.ch>
2019-09-29 00:14:28 +0200

    init commit

---
+++ src/build/precache-manifest.79791af1014447fdf7579e4f130039e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e37fd0e30db53fc6d55e77e47b4d38a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49731562a06d11ec331b",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "a8897975e7d88f12a94c",
+    "url": "/static/js/2.fa304a3e.chunk.js"
+  },
+  {
+    "revision": "49731562a06d11ec331b",
+    "url": "/static/js/main.21785a0a.chunk.js"
+  },
+  {
+    "revision": "71b8d82789c3bd06a793",
+    "url": "/static/js/runtime-main.1d876c17.js"
+  }
+]);
